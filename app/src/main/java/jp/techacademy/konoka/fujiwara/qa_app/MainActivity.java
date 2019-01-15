a2f2244a29fecb54827106ab6b6e06893048f483
konoka <ririmaru2003@gmail.com>
2019-01-15 23:50:35 +0900

    カリキュラム6まで終わり。

--- app/src/main/java/jp/techacademy/konoka/fujiwara/qa_app/MainActivity.java
+++ app/src/main/java/jp/techacademy/konoka/fujiwara/qa_app/MainActivity.java
@@ -1,32 +1,69 @@
 package jp.techacademy.konoka.fujiwara.qa_app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener{
+
+    private Toolbar mToolbar; //ドロワーの為のメンバ
+    private int mGenre = 0; //これよく分からない。
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
+        // --- ここから ---
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+                //FirebaseクラスのgetAuthメソッドで認証情報であるAuthDataクラスのインスタンスを
+                // 取得することができます。
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+                // ログインしていなければログイン画面に遷移させる
+                if (user == null) {
+                    Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                    startActivity(intent);
+                }
             }
         });
+        // --- ここまで修正 ---
+
+
+        /*ナビゲーションドロワーの設定
+        ------------------------------------------------------------------------------------- */
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, mToolbar,
+                R.string.app_name, R.string.app_name);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -49,4 +86,32 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-}
+
+    /*ナビゲーションドロワーの設定 2
+    ------------------------------------------------------------------------------------- */
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.nav_hobby) {
+            mToolbar.setTitle("趣味");
+            mGenre = 1;
+        } else if (id == R.id.nav_life) {
+            mToolbar.setTitle("生活");
+            mGenre = 2;
+        } else if (id == R.id.nav_health) {
+            mToolbar.setTitle("健康");
+            mGenre = 3;
+        } else if (id == R.id.nav_compter) {
+            mToolbar.setTitle("コンピューター");
+            mGenre = 4;
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+
+
+}//mainactivity ここで終わり。
